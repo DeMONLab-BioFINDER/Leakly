1bf88837c2ceb38f5a98498fb447aed69a2ab831
anlijuncn <anlijuncn@gmail.com>
2026-05-13 23:14:48 +0200

    fix log abs path

--- leakly/__init__.py
+++ leakly/__init__.py
@@ -34,7 +34,7 @@ from .permutation import permute_label
 from .simulation import SimulatedDataset, simulate_dataset
 from .summary import SummaryPlotter
 
-__version__ = "0.1.0"
+__version__ = "0.1.1"
 
 __all__ = [
     "__version__",
